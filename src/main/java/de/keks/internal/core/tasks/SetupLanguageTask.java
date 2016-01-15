f679354eeee2affeb52e94ecf1d77f0bb7d29033
Niklas Linz <NiklasLinz@gmx.de>
2016-01-15 13:59:46 +0100

    Fix language on start

--- src/main/java/de/keks/internal/core/tasks/SetupLanguageTask.java
+++ src/main/java/de/keks/internal/core/tasks/SetupLanguageTask.java
@@ -3,7 +3,6 @@ package de.keks.internal.core.tasks;
 import de.keks.cubit.CubitPlugin;
 import de.keks.internal.I18n;
 import de.keks.internal.command.config.ConfigValues;
-import de.keks.internal.command.config.ConfigFile;
 
 /**
  * Copyright:
@@ -28,7 +27,7 @@ public class SetupLanguageTask implements Runnable {
 		cubit.getServer().getScheduler().runTaskAsynchronously(cubit, new Runnable() {
 			@Override
 			public void run() {
-				new I18n(cubit, ConfigFile.getString(ConfigValues.pluginLocale));
+				new I18n(cubit, ConfigValues.pluginLocale);
 
 			}
 		});
